d7cb17580dab61ef1930138f72420ff1251f2a97
jhonvalen <49419935+jhonvalen@users.noreply.github.com>
2019-12-04 10:22:47 -0500

    Added user input changes
    Still need to fix for zip input questions

--- src/edu/upenn/cit594/ui/CommandLineUserInterface.java
+++ src/edu/upenn/cit594/ui/CommandLineUserInterface.java
@@ -1,13 +1,10 @@
 package edu.upenn.cit594.ui;
 
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.util.regex.*;
 
 import edu.upenn.cit594.data.ZipCode;
-import edu.upenn.cit594.processor.LivableAreaMetric;
-import edu.upenn.cit594.processor.MarketValueMetric;
 import edu.upenn.cit594.processor.PopulationProcessor;
 import edu.upenn.cit594.processor.ResidenceProcessor;
 import edu.upenn.cit594.processor.ViolationProcessor;
@@ -17,52 +14,49 @@ public class CommandLineUserInterface {
 	protected ResidenceProcessor residenceProcessor;
 	protected PopulationProcessor populationProcessor;
 	protected ViolationProcessor violationProcessor;
-	protected HashMap<Integer, Double> avgMarketMap;
-	protected HashMap<Integer, Double> avgLivingMap;
-	protected HashMap<Integer, Integer> totalMarketMap;
 	protected int zipInput = 0;
 	
 	public CommandLineUserInterface(ResidenceProcessor rp, PopulationProcessor pr, ViolationProcessor vp) {
 		this.residenceProcessor = rp;
 		this.populationProcessor = pr;
 		this.violationProcessor = vp;
-		this.avgMarketMap = new HashMap<Integer, Double>();
-		this.avgLivingMap = new HashMap<Integer, Double>();
-		this.totalMarketMap = new HashMap<Integer, Integer>();
 	}
 	
-	public void displayUserInput(int userInput) {		
+	private void runUserInput(int userInput) {		
 		switch (userInput) {
+			case 0:
+				System.exit(0);
+				break;
 			case 1:
 				System.out.println(this.populationProcessor.totalZipPopulation());
 				break;
 			case 2:
-				TreeSet<ZipCode> zipTree = this.violationProcessor.zipCodeFinePerCapita();
+				TreeSet<ZipCode> zipTree = this.violationProcessor.storeZipCodestoTree();
 				for (ZipCode zipCode : zipTree) {
 					System.out.println(zipCode.getZipCode() + " " + String.format("%.4f", zipCode.getFinePerCapita()));
 				}
 				break;
 			case 3:
 				this.zipInput = this.storeZipInput();
-				int avgMarketValue = residenceProcessor.residenceAvgCalculation(
-						new MarketValueMetric(), this.zipInput);
+				long avgMarketValue = residenceProcessor.calculateAvgMarketValue(this.zipInput);
 				System.out.println(avgMarketValue);					
 				break;
 			case 4:
 				this.zipInput = this.storeZipInput();
-				int avgLivingArea = residenceProcessor.residenceAvgCalculation(new LivableAreaMetric(), this.zipInput);
+				long avgLivingArea = residenceProcessor.calculateAvgLivingArea(this.zipInput);
 				System.out.println(avgLivingArea);
 				break;
 			case 5:
 				this.zipInput = this.storeZipInput();
-				int totalResidentialPerCapita = residenceProcessor.totalMarketValue(this.zipInput);
+				long totalResidentialPerCapita = residenceProcessor.calculateTotalMarketValue(this.zipInput);
 				System.out.println(totalResidentialPerCapita);
 				break;
 			case 6:
-				System.out.println("user entered 6");
+				System.out.println(residenceProcessor.calculateCorrelation());
 				break;
 			default:
-				System.out.println("user entered invalid number");
+				System.out.println("You have entered a value not between 0 and 6\n"
+						+ "Please enter a number between 0 and 6 to continue");
 				break;
 		}
 	}
@@ -87,5 +81,28 @@ public class CommandLineUserInterface {
 		
 		return zipInt;
 	}
-
+	
+	public void displayUserInput() {
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("Specify what action you would like to run:\n" 
+								+ "Enter 0 to exit\n" + "Enter 1 to show the total population for all Zip Codes\n" 
+								+ "Enter 2 to show the total parking fines per capita for each Zip Code\n"
+								+ "Enter 3 to show the average market value for residences in a specified Zip Code\n"
+								+ "Enter 4 to show the average total livable area for residences in a specified Zip Code\n"
+								+ "Enter 5 to show the total residential market value per capita for a specified Zip Code\n"
+								+ "Enter 6 to show the correlation between population density and average fine amount for all Zip Codes");
+			if(!in.hasNextInt()) {
+				System.out.println("You have entered a value that is not an integer!" + "\n"
+									+ "Enter a number between 0 and 6 to use this program!");
+				System.exit(0);
+				in.close();
+			}
+			
+			int input = in.nextInt();
+			runUserInput(input);
+		}
+				
+	}
 }
